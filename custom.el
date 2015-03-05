39440d99b5ee288580ed394c7c4050898eaa1890
Thomas Sojka <thsojka@mytum.de>
2015-03-05 23:50:55 +0100

    Removed unused customizations

--- custom.el
+++ custom.el
@@ -5,14 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" default)))
  '(ido-save-directory-list-file "~/var/.ido.last")
- '(js2-basic-offset 2)
- '(js2-bounce-indent-p t)
  '(org-agenda-files (quote ("~/test.org")))
  '(recentf-save-file "~/.emacs.d/var/.recentf")
  '(save-place-file "/home/thomas/.emacs.d/var/.places")
- '(send-mail-function nil)
  '(smex-save-file "~/.emacs.d/var/smex-items")
- '(visible-bell t))
+ '(visible-bell t)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
