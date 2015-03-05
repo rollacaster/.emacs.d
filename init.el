225f3e5ec26b08e975cf4035438323183c81e369
Thomas Sojka <thsojka@mytum.de>
2015-03-05 23:39:03 +0100

    Added package installations

--- init.el
+++ init.el
@@ -1,36 +1,96 @@
-;; Use j2mode for js-Files
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;; set path to dependencies
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
 
-;; Disable sounds
-(setq ring-bell-function 'ignore)
+(setq settings-dir
+      (expand-file-name "settings" user-emacs-directory))
 
-;; Disable UI Elements
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(setq theme-dir
+      (expand-file-name "themes" user-emacs-directory))
 
-;; Change keys for faster editing
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\C-c\C-k" 'kill-region)
+(setq custom-file 
+      (expand-file-name "custom.el" user-emacs-directory))
 
-;; Enable forbidden commands
+;; load paths
+(add-to-list 'load-path settings-dir)
+(add-to-list 'load-path site-lisp-dir)
+(add-to-list 'load-path theme-dir)
+(load custom-file)
+
+(require 'appearance)
+
+;; add external projects to load path
+(dolist (project (directory-files site-lisp-dir t "\\w+"))
+  (when (file-directory-p project)
+  (add-to-list 'load-path project)))
+
+;; write backup to own directories
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+           (concat user-emacs-directory "backups")))))
+
+;; make always backup files
+(setq vc-make-backup-files t)
+
+;; save point position
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file (expand-file-name ".places" user-emacs-directory))
+
+
+(require 'sane-defaults)
+(require 'key-bindings)
+
+;; setup packages
+(require 'setup-package)
+
+(defun init-install-packages ()
+  (packages-install
+   '(magit
+     yasnippet
+     smartparens
+     js2-mode
+     dired-details
+     popup
+     tern
+     auto-complete
+     tern-auto-complete
+     guide-key
+     )
+   )
+  )
+
+(condition-case nil
+    (init-install-packages)
+  (error
+   (package-refresh-contents)
+   (init-install-packages)
+  )
+)
+
+;; enable forbidden commands
 (put 'narrow-to-region 'disabled nil)
 (put 'narrow-to-page 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(send-mail-function nil)
- '(visible-bell t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+
+;; setup extensions
+;; (require 'setup-conkeror)
+(eval-after-load 'js2-mode '(require 'setup-js2-mode))
+(eval-after-load 'dired '(require 'setup-dired))
+(require 'setup-yasnippet)
+(require 'setup-autocomplete)
+;;(require 'setup-guide-key)
+(require 'setup-smartparens)
+(require 'setup-tern)
+;; (require 'setup-web-mode)
+(require 'setup-ido)
+(require 'setup-smex)
+
+;; map files to modes
+(require 'mode-mappings)
+
+
+
+
