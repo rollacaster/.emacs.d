07fccba9f42e3130222508c5e59d427d4b9e61b0
Thomas Sojka <thosjka@web.de>
2015-03-19 20:27:50 +0100

    Moved all changing config to var

--- custom.el
+++ custom.el
@@ -1,15 +1,20 @@
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" default)))
- '(ido-save-directory-list-file "~/var/.ido.last")
+ '(ac-comphist-file "d:/.emacs.d/var/ac-comphist.dat")
+ '(custom-safe-themes
+   (quote
+    ("cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" default)))
+ '(ido-save-directory-list-file "~/.emacs.d/var/.ido.last")
  '(org-agenda-files (quote ("~/test.org")))
  '(recentf-save-file "~/.emacs.d/var/.recentf")
  '(save-place-file "/home/thomas/.emacs.d/var/.places")
  '(smex-save-file "~/.emacs.d/var/smex-items")
- '(visible-bell t)
+ '(visible-bell t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
