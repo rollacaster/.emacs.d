07119bd7cf7425ca9305b66df5bb67c4634fe6a1
Thomas Sojka <thsojka@mytum.de>
2015-05-05 09:27:33 +0200

    Change mode mappings

--- settings/mode-mappings.el
+++ settings/mode-mappings.el
@@ -2,8 +2,19 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-hook 'web-mode-hook (lambda () (tern-mode t)))
+(add-hook 'web-mode-hook 'yas-minor-mode)
 
 ;; LaTex mode
+(add-to-list 'auto-mode-alist '("\\.tex\\'" . LaTeX-mode))
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(add-hook 'LaTeX-mode-hook 'auto-fill-mode)
+
+;; Org mode
+(add-hook 'org-capture 'auto-fill-mode)
+(add-hook 'org-capture 'flyspell-mode)
+(add-hook 'org-capture 'ispell)
+
+;; Rest-Client mode
+(add-to-list 'auto-mode-alist '("\\.api\\'" . restclient-mode))
 
 (provide 'mode-mappings)
