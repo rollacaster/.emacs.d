86f3013535d8b7d54dfa5cdacf5eb1a6cc51fddc
Thomas Sojka <thsojka@mytum.de>
2015-05-05 09:27:58 +0200

    Global key for org store link

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -11,7 +11,7 @@
 (global-set-key (kbd "C-x C-m") 'smex)
 
 ;; Org-Mode
-(global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c l") 'org-store-link)
 
 (provide 'key-bindings)
