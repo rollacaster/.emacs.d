d545a0bb5c15bbf4360f89d20c8a6ee335a2ef6b
Thomas Sojka <thsojka@mytum.de>
2015-08-10 20:14:31 +0200

    Add hooks for flyspell and projectile

--- settings/mode-mappings.el
+++ settings/mode-mappings.el
@@ -3,11 +3,13 @@
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-hook 'web-mode-hook (lambda () (tern-mode t)))
 (add-hook 'web-mode-hook 'yas-minor-mode)
+(add-hook 'web-mode-hook 'projectile-mode)
 
 ;; LaTex mode
 (add-to-list 'auto-mode-alist '("\\.tex\\'" . LaTeX-mode))
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
 
 ;; Org mode
 (add-hook 'org-capture 'auto-fill-mode)
