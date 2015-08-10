b02e0148b475382a75820ec9d828344d235304ed
Thomas Sojka <thsojka@mytum.de>
2015-08-10 20:14:01 +0200

    Add support for jsx in web-mode

--- settings/setup-web-mode.el
+++ settings/setup-web-mode.el
@@ -1,5 +1,10 @@
 (require 'web-mode)
 
 (setq web-mode-code-indent-offset 2)
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
 
 (provide 'setup-web-mode)
