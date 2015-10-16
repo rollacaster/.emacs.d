9cfd08e29b8af547be93b49f5b3d53f7af7e679f
Thomas Sojka <thsojka@mytum.de>
2015-10-16 13:33:51 +0200

    Change key for org store link

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -14,7 +14,7 @@
 
 ;; Org-Mode
 (global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c s") 'org-store-link)
 
 ;; Windmove
 (global-set-key (kbd "C-c h")  'windmove-left)
