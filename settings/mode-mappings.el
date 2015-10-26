80f339952d152df12a7140b940416d6d24cfad49
Thomas Sojka <thsojka@web.de>
2015-10-26 21:06:19 +0100

    Use js2-mode for js files

--- settings/mode-mappings.el
+++ settings/mode-mappings.el
@@ -1,5 +1,4 @@
 ;; Web-Mode
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-hook 'web-mode-hook (lambda () (tern-mode t)))
 (add-hook 'web-mode-hook 'yas-minor-mode)
@@ -13,6 +12,16 @@
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+
+;; JS2 Mode
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook 'flycheck-mode)
+(add-hook 'js2-mode-hook 'tern-mode)
+(add-hook 'js2-mode-hook 'yas-minor-mode)
+(add-hook 'js2-mode-hook
+          #'(lambda ()
+              (define-key web-mode-map "\C-ci" 'js-doc-insert-function-doc)
+              (define-key web-mode-map "@" 'js-doc-insert-tag)))
 
 ;; Org mode
 (add-hook 'org-capture 'auto-fill-mode)
