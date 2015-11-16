e26203d7bf679defda70dcc521e959675877b801
Thomas Sojka <thsojka@web.de>
2015-11-16 23:06:49 +0100

    No auto-quotes in web-mode

--- settings/setup-web-mode.el
+++ settings/setup-web-mode.el
@@ -1,6 +1,10 @@
 (require 'web-mode)
 
 (setq web-mode-code-indent-offset 2)
+
+;; No auto quote in html tags
+(setq web-mode-enable-auto-quoting nil)
+
 (defadvice web-mode-highlight-part (around tweak-jsx activate)
   (if (equal web-mode-content-type "jsx")
       (let ((web-mode-enable-part-face nil))
