6412e4f96a688262b26ec529da7921302b17abf0
Thomas Sojka <thomas@Thomass-MBP.fritz.box>
2015-12-24 16:24:12 +0100

    Add capture template

--- settings/setup-org.el
+++ settings/setup-org.el
@@ -2,7 +2,9 @@
 (setq org-capture-templates '(("t" "Todo" entry (file "~/Dropbox/org/Todo.org")
                                "* TODO %?\n  %i\n")
                               ("l" "Learn" entry (file "~/Dropbox/org/Learn.org")
-                               "* %?\n  %i\n")))
+                               "* %?\n  %i\n")
+                              ("c" "comSysto" entry (file "~/Dropbox/org/comSysto.org")
+                               "* TODO %?\n  %i\n")))
 
 ;; Use "⤵" instead of "..." for indicating sub-items
 (setq org-ellipsis "⤵")
