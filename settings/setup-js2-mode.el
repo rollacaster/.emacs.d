075112cde34efcd6bf36479aa706d4769be6c90b
Thomas Sojka <thomas.sojka@comsysto.com>
2016-01-27 18:27:57 +0100

    Document js2 settings

--- settings/setup-js2-mode.el
+++ settings/setup-js2-mode.el
@@ -1,10 +1,16 @@
-(custom-set-variables
- '(js2-basic-offset 2)
- '(js2-bounce-indent-p t)
-)
+;; Activate toggle indent with tab
+(setq js2-bounce-indent-p t)
 
+;; Set basic offset to 2
+(setq js2-basic-offset 2)
+
+;; Highlight as much as possible
 (setq js2-highlight-level 3)
-(setq js2-strict-missing-semi-warning nil)
+
+;; Do not warn about missing semicolons
+(setq js2-strict-missing-semi-warningl nil)
+
+;; Add externals
 (setq js2-global-externs '("describe", "it", "expect" "beforeEach", "sinon"))
 
 (provide 'setup-js2-mode)
