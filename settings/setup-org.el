e93195898328f834f8c8ce808e638eacced8094c
Thomas Sojka <thomas.sojka@comsysto.com>
2016-07-09 20:12:04 +0200

    Use standard org keywords

--- settings/setup-org.el
+++ settings/setup-org.el
@@ -1,19 +1,16 @@
 ;; Org capture templates to add todos or learn actions
-(setq org-capture-templates '(("t" "☛ TODO" entry (file "~/Dropbox/org/Todo.org")
-                               "* ☛ TODO %?\n  %i\n")
+(setq org-capture-templates '(("t" "TODO" entry (file "~/Dropbox/org/Todo.org")
+                               "* TODO %?\n  %i\n")
                               ("l" "Learn" entry (file "~/Dropbox/org/Learn.org")
                                "* %?\n  %i\n")
                               ("c" "comSysto" entry (file "~/Dropbox/org/comSysto.org")
-                               "* ☛ TODO %?\n  %i\n")))
+                               "* TODO %?\n  %i\n")))
 
 ;; Use "⤵" instead of "..." for indicating sub-items
 (setq org-ellipsis "⤵")
 
 ;; Use utf8 org bullets
 (setq org-bullets-bullet-list '("◉" "◎" "⚫" "○" "►" "◇"))
-
-;; Use utf8 todo states
-(setq org-todo-keywords '((sequence "☛ TODO(t)" "✔ DONE(d)")))
 
 ;; Add all files in the org-directory to the agenda
 (setq org-folder "~/Dropbox/org")
