80e9f1789776d0e6c215681771b670069622ee71
Thomas Sojka <thomas.sojka@comsysto.com>
2016-07-09 20:15:35 +0200

    Setup custom variables

--- custom.el
+++ custom.el
@@ -3,9 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(js2-basic-offset 2)
+ '(json-reformat:indent-width 2)
+ '(magit-log-arguments (quote ("--graph" "--decorate" "-n256")))
+ '(org-agenda-files
+   (quote
+    ("/Users/thomas/Dropbox/org/Address.org" "/Users/thomas/Dropbox/org/Dates.org" "/Users/thomas/Dropbox/org/Debt.org" "/Users/thomas/Dropbox/org/Geld.org" "/Users/thomas/Dropbox/org/Learn.org" "/Users/thomas/Dropbox/org/Movies.org" "/Users/thomas/Dropbox/org/Presents.org" "/Users/thomas/Dropbox/org/Projects.org" "/Users/thomas/Dropbox/org/Todo.org" "/Users/thomas/Dropbox/org/Tools.org" "/Users/thomas/Dropbox/org/Urlaub.org" "/Users/thomas/Dropbox/org/Wishlist.org" "/Users/thomas/Dropbox/org/Wohnung.org" "/Users/thomas/Dropbox/org/comSysto.org")))
  '(safe-local-variable-values
    (quote
-    ((mocha-options . "--compilers js:./test/compiler;")
+    ((setq js2-basic-offset 4)
+     (mocha-options . "--reporter dot --r src/test/setup.js --compilers js:babel-core/register")
+     (mocha-options . "--reporter dot --r babel-core/register --r src/test/setup.js")
+     (mocha-options . "--R dot --r babel-core/register --r src/test/setup.js")
+     (mocha-options . "src/test/setup.js")
+     (mocha-options . src/test/setup\.js)
+     (setq js2-basic-offset 2)
+     (flycheck-checker . javascript-standand)
+     (mocha-options . "--compilers js:./test/compiler;")
      (mocha-project-test-directory . "src/**/*.spec.js?(x)")
      (mocha-project-test-directory . "**/*.spec.js")
      (mocha-options . "--compilers --require babel-core/register")
