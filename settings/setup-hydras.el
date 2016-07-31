ebefa30e2d483c6edeb575d21f9c30b6e277fcef
Thomas Sojka <thomas.sojka@comsysto.com>
2016-07-31 17:54:29 +0200

    Change coloring hydra

--- settings/setup-hydras.el
+++ settings/setup-hydras.el
@@ -1,16 +1,11 @@
-(defhydra hydra-hue (css-mode-map "C-c C-h")
-  "color-hue"
-  ("i" kurecolor-increase-hue-by-step "increase hue")
-  ("d" kurecolor-decrease-hue-by-step "decreasee hue"))
-
-(defhydra hydra-brightness (css-mode-map "C-c C-b")
-  "color-brightness"
-  ("i" kurecolor-increase-brightness-by-step "increase brightness")
-  ("d" kurecolor-decrease-brightness-by-step "decrease brightness"))
-
-(defhydra hydra-saturation (css-mode-map "C-c C-s")
-  "color-saturation"
+(eval-after-load 'css-mode '(defhydra hydra-color (css-mode-map "C-c C-c")
+  "color"
+  ("u" kurecolor-increase-hue-by-step "increase hue")
+  ("j" kurecolor-decrease-hue-by-step "decreasee hue")
   ("i" kurecolor-increase-saturation-by-step "increase saturation")
-  ("d" kurecolor-decrease-saturation-by-step "decrease saturation"))
+  ("k" kurecolor-decrease-saturation-by-step "decrease saturation")
+  ("o" kurecolor-increase-brightness-by-step "increase brightness")
+  ("l" kurecolor-decrease-brightness-by-step "decrease brightness")))
+
 
 (provide 'setup-hydras)
