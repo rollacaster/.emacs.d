90084cda764364eeb5976d623318400e76436e43
Thomas Sojka <thomas.sojka@comsysto.com>
2017-01-06 12:44:09 +0100

    Show marked line

--- settings/appearance.el
+++ settings/appearance.el
@@ -8,4 +8,8 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
+(global-hl-line-mode 1)
+
+(setq redisplay-dont-pause t)
+
 (provide 'appearance)
