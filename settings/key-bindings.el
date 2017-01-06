45d6aceb32cebb3725620096c1e29e3e4d824fcb
Thomas Sojka <thomas.sojka@comsysto.com>
2017-01-06 12:44:25 +0100

    Change helm keys

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -17,7 +17,8 @@
 (global-set-key (kbd "C-c i o") 'helm-occur)
 (global-set-key (kbd "C-c i g") 'helm-google-suggest)
 
-(global-set-key (kbd "C-c i s") 'helm-spotify)
+(global-set-key (kbd "C-x s") 'helm-spotify)
+(global-set-key (kbd "C-x a") 'helm-execute-kmacro)
 (global-set-key (kbd "C-x C-m") 'helm-M-x)
 (global-set-key (kbd "C-x m") 'helm-M-x)
 (global-set-key (kbd "C-x b") 'helm-mini)
