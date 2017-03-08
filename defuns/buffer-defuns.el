e936902cb49e9425697a7b1d54c6c7b96d8d2f3d
Thomas Sojka <thomas.sojka@comsysto.com>
2017-03-08 08:58:26 +0100

    Add function to quite side windows

---
+++ defuns/buffer-defuns.el
@@ -0,0 +1,7 @@
+(defun lunaryorn-quit-bottom-side-windows ()
+  "Quit side windows of the current frame."
+  (interactive)
+  (dolist (window (window-at-side-list))
+    (quit-window nil window)))
+
+(global-set-key (kbd "C-c q") #'lunaryorn-quit-bottom-side-windows)
