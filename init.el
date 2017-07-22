d1c870c5ed904831ef1a1a428bd8370209cff4ad
Thomas Sojka <thomas.sojka@comsysto.com>
2017-07-22 23:45:16 +0200

    install auctex & web-page via use-packge

--- init.el
+++ init.el
@@ -46,11 +46,7 @@
 (defun init-install-packages ()
   (packages-install
    '(
-     popup
-     texinfo
-     auctex
-     ispell
-     web-mode
+
      rainbow-mode
      use-package
      )
@@ -102,8 +98,6 @@
   :bind (( "C-x g" . magit-status)
          ( "C-x M-g" . magit-dispatch-popup)))
 (eval-after-load 'dired '(require 'setup-dired))
-(require 'setup-auctex)
-(require 'setup-web-mode)
 (use-package noflet)
 (require 'setup-org)
 (use-package neotree
@@ -318,3 +312,32 @@
     :config
     (js2r-add-keybindings-with-prefix "C-c C-j"))
   (use-package xref-js2))
+
+(use-package web-mode
+  :config
+  (setq web-mode-code-indent-offset 2)
+  ;; No auto quote in html tags
+  (setq web-mode-enable-auto-quoting nil)
+
+  (defadvice web-mode-highlight-part (around tweak-jsx activate)
+    (if (equal web-mode-content-type "jsx")
+        (let ((web-mode-enable-part-face nil))
+          ad-do-it)
+      ad-do-it))
+
+  (eval-after-load 'web-mode
+    '(define-key web-mode-map (kbd "C-c C-v") 'browse-url-of-file))
+
+  (eval-after-load 'web-mode
+    '(define-key web-mode-map (kbd "C-c RET") 'mc/mark-all-like-this-dwim))
+
+  (define-key web-mode-map (kbd "C-;") 'company-web-html)
+  (add-hook 'web-mode-hook (lambda ()
+                             (set (make-local-variable 'company-backends) '(company-web-html company-files))
+                             (company-mode t))))
+
+(use-package auctex
+  :defer t
+  :config
+  ;; Make AUCTex aware of style files and multi-files
+  (setq TeX-parse-self t))
--- settings/setup-auctex.el
+++
@@ -1,7 +0,0 @@
-
-;; Make AUCTex aware of style files and multi-files
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-(setq-default TeX-master nil)
-
-(provide 'setup-auctex)
