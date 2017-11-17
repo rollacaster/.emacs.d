5f1ab6b3782b65f3c7c6c1bc456ae99d5498c90b
Thomas Sojka <thomas.sojka@comsysto.com>
2017-11-17 21:58:33 +0100

    Updat dired setup

--- settings/setup-dired.el
+++ settings/setup-dired.el
@@ -1,5 +1,4 @@
 (require 'dired)
-(require 'dash)
 
 ;; Make dired less verbose
 (use-package dired-details)
@@ -17,36 +16,17 @@
   (eval `(defadvice ,it (after revert-buffer activate)
            (revert-buffer))))
 
+;; Easier delte in dired
+(define-key dired-mode-map (kbd "C-a") 'dired-back-to-start-of-files)
+(define-key dired-mode-map (kbd "k") 'dired-do-delete)
+
 ;; C-a is nicer in dired if it moves back to start of files
 (defun dired-back-to-start-of-files ()
   (interactive)
   (backward-char (- (current-column) 2)))
-
-(define-key dired-mode-map (kbd "C-a") 'dired-back-to-start-of-files)
-(define-key dired-mode-map (kbd "k") 'dired-do-delete)
-
-;; M-up is nicer in dired if it moves to the fourth line - the first file
-(defun dired-back-to-top ()
-  (interactive)
-  (beginning-of-buffer)
-  (dired-next-line 4))
-(define-key dired-mode-map (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
-(define-key dired-mode-map (vector 'remap 'smart-up) 'dired-back-to-top)
-
-;; M-down is nicer in dired if it moves to the last file
-(defun dired-jump-to-bottom ()
-  (interactive)
-  (end-of-buffer)
-  (dired-next-line -1))
-(define-key dired-mode-map (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
-(define-key dired-mode-map (vector 'remap 'smart-down) 'dired-jump-to-bottom)
-
-;; Delete with C-x C-k to match file buffers and magit
 (define-key dired-mode-map (kbd "C-x C-k") 'dired-do-delete)
 (eval-after-load "wdired"
-  '(progn
-     (define-key wdired-mode-map (kbd "C-a") 'dired-back-to-start-of-files)
-     (define-key wdired-mode-map (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
-     (define-key wdired-mode-map (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)))
+     '(define-key wdired-mode-map (kbd "C-a") 'dired-back-to-start-of-files))
  
+
 (provide 'setup-dired)
