ad1aad47355a6888e981da0cb2a1272196d37b3f
Thomas Sojka <thomas.sojka@comsysto.com>
2017-11-18 13:02:14 +0100

    Remove unused funs

--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -1,11 +1,3 @@
-(defun lunaryorn-quit-bottom-side-windows ()
-  "Quit side windows of the current frame."
-  (interactive)
-  (dolist (window (window-at-side-list))
-    (quit-window nil window)))
-
-(global-set-key (kbd "C-c q") #'lunaryorn-quit-bottom-side-windows)
-
 (defun copy-file-path ()
   "copy buffer's full path to kill ring"
   (interactive)
