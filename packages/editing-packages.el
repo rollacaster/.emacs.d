b763512f25d57664901e368c3538ed8931050c1f
Thomas Sojka <thomas.sojka@comsysto.com>
2017-11-22 10:48:07 +0100

    Rebind avy-goto-char

--- packages/editing-packages.el
+++ packages/editing-packages.el
@@ -54,7 +54,7 @@
 
 (use-package avy
   :bind (( "C-c g" . avy-goto-line)
-         ( "C-c v" . avy-goto-char)
+         ( "C-'" . avy-goto-char)
          ( "C-c ," . avy-copy-line)
          ( "C-c m" . avy-move-line)
          ( "C-c ." . avy-copy-region)))
