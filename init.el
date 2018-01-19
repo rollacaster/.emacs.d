75dbb7eccb6ef38632c86c206e5a81159f24ef41
Thomas Sojka <thomas.sojka@comsysto.com>
2018-01-19 22:37:06 +0100

    Add org-archive

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("org" . "https://orgmode.org/elpa/"))
 
 (package-initialize)
 
