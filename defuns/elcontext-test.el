f90c4ce3deb3285b265267e62028fad61e050536
Thomas Sojka <thomas.sojka@comsysto.com>
2018-01-19 22:35:32 +0100

    Remove elcontext from config
    Moved to own repo

--- defuns/elcontext-test.el
+++
@@ -1,9 +0,0 @@
-;;; elcontext --- Define context specific services in emacs
-;;; Commentary:
-;;; Code:
-(require 'ht)
-
-(ert-deftest elc-store-context-test ()
-  "Store a new location")
-
-;;; elcontext-test.el ends here
