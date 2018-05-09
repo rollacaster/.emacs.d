117bafed6a53d9843cf8710ff944595fadc69dcd
Thomas Sojka <thomas.sojka@comsysto.com>
2018-05-09 15:10:41 +0200

    Add week numbers to calendar

--- settings/appearance.el
+++ settings/appearance.el
@@ -11,4 +11,16 @@
 (set-face-attribute 'default nil :height 150)
 
 (set-frame-name "🐦")
+
+;; Add week numbers to calendar
+(copy-face font-lock-constant-face 'calendar-iso-week-face)
+(set-face-attribute 'calendar-iso-week-face nil
+                    :height 0.7)
+(setq calendar-intermonth-text
+      '(propertize
+        (format "%2d"
+                (car
+                 (calendar-iso-from-absolute
+                  (calendar-absolute-from-gregorian (list month day year)))))
+        'font-lock-face 'calendar-iso-week-face))
 (provide 'appearance)
