5b63f5085d34b2161a23e55f3b347802905ebdf5
Thomas Sojka <thomas.sojka@comsysto.com>
2018-05-14 08:04:11 +0200

    Add clojure setup

--- init.el
+++ init.el
@@ -621,6 +621,42 @@
 
 (use-package vmd-mode)
 
+(use-package eldoc)
+(use-package cider
+  :bind (:map cider-mode-map
+              ("C-c RET" . mc/mark-all-like-this-dwim))
+  :config
+  (setq cider-repl-history-file "~/.emacs.d/cider-history")
+  (setq cider-repl-wrap-history t)
+  (add-hook 'cider-repl-mode-hook 'paredit-mode)
+  (add-hook 'cider-repl-mode-hook #'eldoc-mode)
+  (add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
+  (add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode)
+  (setq cider-show-error-buffer nil)
+  (setq cider-auto-select-error-buffer nil)
+  (setq cider-repl-pop-to-buffer-on-connect t)
+  (setq cider-repl-use-pretty-printing t))
+
+(use-package clj-refactor
+  :config
+  (setq cljr-warn-on-eval nil)
+  (cljr-add-keybindings-with-prefix "C-c C-r"))
+
+(use-package clojure-mode
+  :config
+  (add-hook 'clojure-mode-hook 'enable-paredit-mode)
+  (use-package clojure-mode-extra-font-locking)
+  ;; Use clojure mode for other extensions
+  (add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+  (add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
+  (add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
+  (add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode)))
+
+(use-package rainbow-delimiters
+  :config
+  (rainbow-delimiters-mode))
+
 (use-package dictcc)
 
 (load-file "./frame-move.el")
