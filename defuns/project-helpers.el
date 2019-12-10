c8dc1234786ded2ba6a830e77b4c7d1e63596bb8
Thomas Sojka <thomas.sojka@comsysto.com>
2019-12-10 07:36:06 +0100

    Create rac-open-localhost

--- defuns/project-helpers.el
+++ defuns/project-helpers.el
@@ -2,3 +2,8 @@
   (interactive)
   (let ((server (completing-read "Server:" (mapcar (function first) sql-connection-alist))))
     (sql-connect server)))
+
+(defun rac-open-localhost ()
+  (interactive)
+  (let ((port (read-number "Port:" )))
+    (browse-url "http://localhost:")))
