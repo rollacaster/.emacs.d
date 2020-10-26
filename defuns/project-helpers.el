0ab8595c1affe6001293c5ce7e401f00abfe7cbf
Thomas Sojka <thomas.sojka@comsysto.com>
2020-10-26 09:04:52 +0100

    Add store mail defun

--- defuns/project-helpers.el
+++ defuns/project-helpers.el
@@ -15,3 +15,23 @@
            (list (concat "https://github.com/rollacaster/" project ".git")
                  (concat "/Users/thomas/projects/" project)
                  nil))))
+
+(defun rac-store-mail (&optional dir)
+  "Copy message at point to somewhere else as <date>_<subject>.eml."
+  (interactive)
+  (let* ((msg (mu4e-message-at-point))
+         (target (format "%s_%s.eml"
+                         (format-time-string "%F" (mu4e-message-field msg :date))
+                         (or (mu4e-message-field msg :subject) "No subject"))))
+    (copy-file
+     (mu4e-message-field msg :path)
+     (concat
+      "~/Downloads/email-"
+      (format-time-string "%Y-%m-%dT%T")
+      ((lambda (x) (concat (substring x 0 3) ":" (substring x 3 5)))
+       (format-time-string "%z"))))))
+
+
+
+
+
