ab5791a5354982fcb8878db51cfbd40590a73026
Thomas Sojka <thsojka@web.de>
2021-01-21 09:45:40 +0100

    Update roam bookmark

--- roam-bookmark.js
+++ roam-bookmark.js
@@ -5,14 +5,34 @@ javascript: location.href =
   encodeURIComponent(document.title) +
   '&body=' +
   encodeURIComponent(window.getSelection()) +
-  (document.querySelector("head > meta[itemprop='author'") ||
-    (document.querySelector('head > script[type="application/ld+json"]') &&
-      ('&author=' +
-        document.querySelector("head > meta[itemprop='author'").content ||
+  (document.querySelector("head > meta[itemprop='author'")
+    ? '&author=' +
+      document.querySelector("head > meta[itemprop='author'").content
+    : document.querySelector('head > script[type="application/ld+json"]') &&
+      Array.isArray(
         JSON.parse(
           document.querySelector('head > script[type="application/ld+json"]')
             .innerText
         )
-          .find((schema) => schema.author)
-          .author.map((a) => a.name)
-          .join(' '))))
+      )
+    ? '&author=' +
+      Array.isArray(
+        JSON.parse(
+          document.querySelector('head > script[type="application/ld+json"]')
+            .innerText
+        )
+      )
+        .find((schema) => schema.author)
+        .author.map((a) => a.name)
+        .join(' ')
+    : document.querySelector('head > script[type="application/ld+json"]') &&
+      JSON.parse(
+        document.querySelector('head > script[type="application/ld+json"]')
+          .innerText
+      ).author
+    ? '&author=' +
+      JSON.parse(
+        document.querySelector('head > script[type="application/ld+json"]')
+          .innerText
+      ).author
+    : '')
